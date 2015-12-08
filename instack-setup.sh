9da2ef766706f524396c90e79839771972e4263e
Ryan Hallisey <rhallise@redhat.com>
2015-12-08 08:21:12 -0500

    Update instack script

--- instack-setup.sh
+++ instack-setup.sh
@@ -11,10 +11,10 @@ sudo virsh undefine baremetalbrbm_1
 sudo virsh undefine baremetalbrbm_2
 sudo virsh undefine baremetalbrbm_3
 
-export NODE_DIST=centos7
-export DIB_LOCAL_IMAGE=CentOS-7-x86_64-GenericCloud-1508.qcow2
-#export NODE_DIST=rhel7
-#export DIB_LOCAL_IMAGE=rhel-guest-image-7.2-20150925.0.x86_64.qcow2
+#export NODE_DIST=centos7
+#export DIB_LOCAL_IMAGE=CentOS-7-x86_64-GenericCloud-1508.qcow2
+export NODE_DIST=rhel7
+export DIB_LOCAL_IMAGE=rhel-guest-image-7.2-20151102.0.x86_64.qcow2
 export NODE_COUNT=4
 export NODE_CPU=4
 export NODE_MEM=16384
@@ -29,3 +29,5 @@ echo "Waiting for VM to start..."
 scp $DIB_LOCAL_IMAGE root@${INSTACK_IP}:/home/stack
 scp undercloud-setup.sh root@${INSTACK_IP}:/home/stack
 scp overcloud-containers-setup.sh root@${INSTACK_IP}:/home/stack
+scp net-single-nic-with-vlans.yaml root@${INSTACK_IP}:/home/stack
+scp  setup-undercloud-route.sh root@${INSTACK_IP}:/home/stack
