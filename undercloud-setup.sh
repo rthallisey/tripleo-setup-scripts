cff99e3ff9545a81ea4bb28d7ebc49dbd28bcc88
Ryan Hallisey <rhallise@redhat.com>
2016-01-15 12:29:28 -0500

    Point the undercloud at delorean current

--- undercloud-setup.sh
+++ undercloud-setup.sh
@@ -3,6 +3,8 @@
 git clone https://git.openstack.org/openstack/tripleo-common
 
 export DIB_INSTALLTYPE_puppet_modules=source
+export DELOREAN_REPO_URL=http://trunk.rdoproject.org/centos7/current/
+
 ./tripleo-common/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
 
 # Bug with introspecting nodes
