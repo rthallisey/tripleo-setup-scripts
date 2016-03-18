603eaa1bdc9b4ed76fde37f2209f37123d237de7
Ryan Hallisey <rhallise@redhat.com>
2016-03-18 16:36:07 -0400

    tripleo.sh has moved to tripleo-ci

--- undercloud-setup.sh
+++ undercloud-setup.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-git clone https://git.openstack.org/openstack/tripleo-common
+git clone https://git.openstack.org/openstack/tripleo-ci
 
 export DIB_INSTALLTYPE_puppet_modules=source
 export DELOREAN_REPO_URL=http://trunk.rdoproject.org/centos7/current/
 
-./tripleo-common/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
+./tripleo-ci/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
 
 ./overcloud-containers-setup.sh
