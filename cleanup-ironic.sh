93dca4c64550ce019ad3fd0e582963cfddf0bded
Ryan Hallisey <rhallise@redhat.com>
2016-03-22 10:19:57 -0400

    Forgot the 's' in openstack

--- cleanup-ironic.sh
+++ cleanup-ironic.sh
@@ -4,5 +4,5 @@ for node in `ironic node-list --fields uuid | tail -n +4 | head -n -1 | cut -d '
     ironic node-update $node remove instance_uuid
 done
 
-sudo systemctl restart opentack-ironic-conductor
-sudo systemctl restart opentack-nova-compute
+sudo systemctl restart openstack-ironic-conductor
+sudo systemctl restart openstack-nova-compute
