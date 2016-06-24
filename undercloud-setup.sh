9d6e43967bae95a7024470363af87527f71c2f2b
Ryan Hallisey <rhallise@redhat.com>
2016-06-24 18:07:17 -0400

    Update the undercloud setup script to accept releases

--- undercloud-setup.sh
+++ undercloud-setup.sh
@@ -1,10 +1,30 @@
 #/bin/bash
 
-git clone https://github.com/openstack-infra/tripleo-ci
+if [ -n $1 ]; then
+    sudo curl -o /etc/yum.repos.d/delorean-$1.repo https://trunk.rdoproject.org/centos7-$1/current/delorean.repo
+    sudo curl -o /etc/yum.repos.d/delorean-deps-$1.repo http://trunk.rdoproject.org/centos7-$1/delorean-deps.repo
+    sudo yum -y install yum-plugin-priorities
+    sudo yum install -y python-tripleoclient
 
-export DIB_INSTALLTYPE_puppet_modules=source
-export DELOREAN_REPO_URL=http://trunk.rdoproject.org/centos7/current/
+    openstack undercloud install
+    source stackrc
 
-./tripleo-ci/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
+    export USE_DELOREAN_TRUNK=1
+    export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7-$1/current/"
+    export DELOREAN_REPO_FILE="delorean.repo"
+
+    openstack overcloud image build --all
+    openstack overcloud image upload
+
+    openstack baremetal import instackenv.json --json
+    openstack baremetal configure boot
+    openstack baremetal introspection bulk start
+else
+    git clone https://github.com/openstack-infra/tripleo-ci
+
+    export DIB_INSTALLTYPE_puppet_modules=source
+    export DELOREAN_REPO_URL=http://trunk.rdoproject.org/centos7/current/
+    ./tripleo-ci/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
+fi
 
 ./overcloud-containers-setup.sh
