e23ebe2f67bae2ff5d2a1d664f7b34b36c969670
Ryan Hallisey <rhallise@redhat.com>
2016-09-15 15:08:05 -0400

    Add the option to run from a stable branch

--- undercloud-setup.sh
+++ undercloud-setup.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-if [[ -n $1 ]]; then
+git clone https://github.com/openstack-infra/tripleo-ci
+
+if [[ $1 = "--stable" ]]; then
+    export STABLE_RELEASE=$1
+    ./tripleo-ci/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
+elif [[ -n $1 ]]; then
     sudo curl -o /etc/yum.repos.d/delorean.repo http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-$1/delorean.repo
     sudo curl -o /etc/yum.repos.d/delorean-deps.repo http://trunk.rdoproject.org/centos7/delorean-deps.repo
 
@@ -21,8 +26,6 @@ if [[ -n $1 ]]; then
     openstack baremetal configure boot
     openstack baremetal introspection bulk start
 else
-    git clone https://github.com/openstack-infra/tripleo-ci
-
     export DIB_INSTALLTYPE_puppet_modules=source
     export DELOREAN_REPO_URL=http://trunk.rdoproject.org/centos7/current/
     ./tripleo-ci/scripts/tripleo.sh --repo-setup --undercloud --overcloud-images --register-nodes --introspect-nodes
